3a1e43144a0df9c2ac7640819b7aeeb450567695
tufailahmed023 <tufailahmed023@gmail.com>
2022-07-20 00:29:34 +0530

    my first push on git

---
+++ gg.py
@@ -0,0 +1,14 @@
+import mysql.connector as conn
+mydp=conn.connect(host='localhost',user='root',passwd='tufailahmed023')
+print(mydp)
+cursor=mydp.cursor()
+cursor.execute("show databases")
+print(cursor.fetchall())
+cursor.execute("use inuron ")
+
+cursor.execute("show tables ")
+print(cursor.fetchall())
+
+cursor.execute("select * from bank_details where age> 35 limit 10")
+for i in cursor.fetchall():
+    print(i)
